ce837e60c48ec06b5e6d15efff7792e9c1e8bab5
amine <amine.benhalloum@gmail.com>
2017-02-23 20:56:32 +0100

    remove stuff

--- parser.py
+++ parser.py
@@ -7,19 +7,13 @@ class Video:
         self.id = id
         self.size = size
 
+
 class Cache:
     def __init__(self, id: int, capacity):
         self.id = id
         self.capacity = capacity
         self.current_capacity = 0
 
-
-
-class Endpoint:
-    def __init__(self, id: int, dc_latency, caches: Dict[Cache, int]):
-        self.id = id
-        self.dc_latency = dc_latency
-        self.caches = caches
 
 class Endpoint:
     def __init__(self, id: int, dc_latency, caches_latency: Dict[Cache, int]):
@@ -55,6 +49,7 @@ class Problem:
         self.endpoints = endpoints
         self.caches = caches
         self.requests = requests
+
 
 def parse(inFile):
     inF = open(inFile, 'r')
